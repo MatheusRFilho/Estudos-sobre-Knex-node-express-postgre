6b99bdeb307c6f62cadbde6009da329b4412331b
Matheus <matheusrodrigofilho@outlook.com>
2020-12-08 10:40:10 -0300

    Criando um not found

--- src/server.js
+++ src/server.js
@@ -6,6 +6,12 @@ const routes= require('./routes')
 app.use(express.json())
 app.use(routes)
 
+//notFound
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
 
 //Catch all
 app.use((error, req, res, next) => {
